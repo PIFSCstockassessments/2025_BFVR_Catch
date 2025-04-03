06f37ecfc55760b7af19df369dceeff4b2fb0342
Marc Nadon <marc.nadon@noaa.gov>
2025-04-03 09:51:17 -1000

    Update BFVR_Catch_Shinny.r
    Added the Lamson quantiles and re-named a few variables for clarity.

--- 02_Scripts/BFVR_Catch_Shinny.r
+++ 02_Scripts/BFVR_Catch_Shinny.r
@@ -3,27 +3,36 @@ pacman::p_load("tidyverse","data.table","this.path","readxl","ggthemes")
 root_dir <- here(..=1)
 
 #============User inputs=================================
-# Assumed proportion of BF-registered boats that caught at least one D7 
-# in a given year.
-prop_caught_d7 <- 1
+# Multiplier on the number of active BFVR non-commercial boats, to take into
+# account the number of unregistered boats.
+multiplier_unregistered <- 1
+
+# Multiplier to BF-registered boats to remove inactive vessels
+# (i.e. not catching deep7) in a given year.
+multiplier_inactive <- 1
 
 # FRS fishers to filter out
 only_bf_registered  <- "Y"  # Only keep BF-registered fishers
 remove_nod7_catch   <- "Y"  # Only keep fishers that caught a deep7
-min_d7_catch        <- 0    # Only keep fishers that caught at least this D7 lbs.
 
-# MRIP interview quantile to filter at
+# MRIP interview and Lamson annual quantile to filter at
 selected.quantile <- c("q90","q95","q99","max")[3]
+
 #=======================================================
 
 #================Load data==============================
-# Load trip-level limits outputted from 04_MRIP_Interview_Catch.qmd
+# Load trip-level limits outputted from 02_MRIP_Interview_Catch.qmd
 # and select the quantile to use.
-QT <- fread(fs::path(root_dir,"03_Outputs","mrip_quantiles.csv")) %>% 
+QT.mrip <- fread(fs::path(root_dir,"03_Outputs","mrip_quantiles.csv")) %>% 
   pivot_longer(cols=q90:max,names_to="quantiles",values_to="value") %>% 
   filter(quantiles==selected.quantile) %>% as.data.table()
 
-SP.id <- QT[,sci_name:sp_frs_id]
+SP.id <- QT.mrip[,sci_name:sp_frs_id]
+
+# Load annual-level limits from 03_Lamson_Annual_Catch.qmd
+QT.lamson <- fread(fs::path(root_dir,"03_Outputs","lamson_quantiles.csv")) %>% 
+  pivot_longer(cols=q90:max,names_to="quantiles",values_to="value") %>% 
+  filter(quantiles==selected.quantile) %>% as.data.table()
 
 # Load fisher counts outputted from 01_Registered_fisher_list.qmd.
 # Appy the correction related to the assume # of BF-registered fishers catching
@@ -38,17 +47,23 @@ F2 <- fread(fs::path(root_dir,"03_Outputs","FRS_trips.csv"))
 # Load CML_catches
 cml <- fread(fs::path(root_dir, "03_Outputs", "CML_catches.csv"))
 
+#======================Modify the number of active BFVR vessels===========
+
+FC$n_bf_fishers <- FC$n_bf_fishers*multiplier_unregistered*multiplier_inactive
+
+#=========================================================================
+
 #======================Filters================================
 # Apply trip-level filters to classify fishers as comm. (1) vs non-comm. (0).
 F2$trip_type <- 0
 
-F2[d7  > QT[sp_frs_id=="d7"]$value|
-   s17 > QT[sp_frs_id=="s17"]$value| 
-   s19 > QT[sp_frs_id=="s19"]$value| 
-   s21 > QT[sp_frs_id=="s21"]$value|
-   s22 > QT[sp_frs_id=="s22"]$value|
-   s97 > QT[sp_frs_id=="s97"]$value| 
-   s20 > QT[sp_frs_id=="s20"]$value 
+F2[d7  > QT.mrip[sp_frs_id=="d7"]$value|
+   s17 > QT.mrip[sp_frs_id=="s17"]$value| 
+   s19 > QT.mrip[sp_frs_id=="s19"]$value| 
+   s21 > QT.mrip[sp_frs_id=="s21"]$value|
+   s22 > QT.mrip[sp_frs_id=="s22"]$value|
+   s97 > QT.mrip[sp_frs_id=="s97"]$value| 
+   s20 > QT.mrip[sp_frs_id=="s20"]$value 
    ]$trip_type <- 1
 
 # Sum catches from trip-level data to annual-level per fisher.
@@ -57,13 +72,26 @@ F3 <- F2 %>% group_by(year, cml_no.fs, bf_registr, cml_registr, county) %>%
 
 # Re-classify "trip_type" into "fisher_type".
 F3 <- F3 %>% mutate(fisher_type=if_else(trip_type > 0,"Comm","NC")) %>%
-  select(-trip_type)
+  select(-trip_type) %>% as.data.table()
+
+# Apply the annual-level filters to classify fishers as comm. vs non-comm.
+F3$annual_type <- "NC"
+
+F3[d7  > QT.lamson[sp_frs_id=="d7"]$value|
+   s17 > QT.lamson[sp_frs_id=="s17"]$value| 
+   s19 > QT.lamson[sp_frs_id=="s19"]$value| 
+   s21 > QT.lamson[sp_frs_id=="s21"]$value|
+   s22 > QT.lamson[sp_frs_id=="s22"]$value|
+   s97 > QT.lamson[sp_frs_id=="s97"]$value| 
+   s20 > QT.lamson[sp_frs_id=="s20"]$value 
+   ]$annual_type <- "Comm"
+
+F3 <- F3 %>% mutate(fisher_type=if_else(annual_type=="Comm","Comm",fisher_type))
 
 # Apply filters on the fishers.
 if(only_bf_registered=="Y"){ F3 <- F3 %>% filter(bf_registr=="Y") }
 if(remove_nod7_catch=="Y") { F3 <- F3 %>% filter(d7!=0) }
 
-F3 <- F3 %>% filter(d7>=min_d7_catch)
 F3 <- F3 %>% filter(fisher_type=="NC")
 #==========================================================
 
@@ -124,7 +152,8 @@ bind_rows(cml.all.sp) %>%
 left_join(SP.id, by = c("species" = "sp_frs_id")) %>% 
 filter(species != "d7") %>% 
 ggplot()+
-      geom_bar(aes(x = year, y=catch, group = type, fill = type), position="stack", stat="identity") +
+      geom_bar(aes(x = year, y=catch, group = type, fill = type),
+               position="stack", stat="identity") +
       theme_minimal()+
       theme(axis.text.x=element_text(angle=45,hjust=1),
             strip.text = element_text(size = 14))+
