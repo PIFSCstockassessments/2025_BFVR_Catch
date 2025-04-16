728f527a34e9b7f53f1eecd5eda930bbff0b1be1
MOshima-PIFSC <megumi.oshima@noaa.gov>
2025-04-16 13:32:47 -1000

    make bar plots layered and add number of fishers plot

--- 04_Shiny/server.r
+++ 04_Shiny/server.r
@@ -121,6 +121,82 @@ F3 <- reactive({
   data_prep()$F3
 })
 
+output$n_bf_fishers_plot <- renderPlotly({
+  F3 <- F3()
+
+  total_fishers <- F3 %>% 
+    distinct(year, county, n_bf_fishers) %>%
+    filter(year < 2023) %>%
+    group_by(year) %>%
+    mutate(total_fishers = sum(n_bf_fishers)) %>%
+    distinct(year, total_fishers) 
+  county_fishers <- F3 %>% 
+    distinct(year, county, n_bf_fishers) %>% 
+    filter(year < 2023)  
+  
+  n_fishers_plot <- plot_ly() %>%
+  add_trace(
+    type = "bar",
+    data = total_fishers, 
+    x = ~year, 
+    y = ~total_fishers,
+    name = "Total number of BF fishers", 
+    marker = list(color = "#D3D3D3"),
+    width = 0.8
+  )
+
+  segments <- unique(county_fishers$county) 
+  all_years <- sort(unique(county_fishers$year))
+  colors <- c("Kauai County" = "#002364",
+  "Maui County" = "#317F3F", "Hawaii County" = "#826087",
+  "Honolulu County" = "#B71300")
+ 
+  for (segment in segments) {
+
+  segment_data <- filter(county_fishers, county == segment) %>%
+                  mutate(offset = case_when(
+                    county == "Honolulu County" ~ -.4, 
+                    county == "Hawaii County" ~ -.2,
+                    county == "Maui County" ~ 0,
+                    county == "Kauai County" ~ .2,
+                    ))
+
+  n_fishers_plot <- n_fishers_plot %>%
+    add_trace(
+      type = "bar",
+      x = segment_data$year,
+      y = segment_data$n_bf_fishers,
+      name = segment_data$county,
+      marker = list(color = colors[segment_data$county],
+                    line = list(color = "#D3D3D3", width = 1)),
+      width = 0.2,
+      offset =  segment_data$offset
+      # hoverinfo = "text",
+      # hovertext = paste0("Year: ", segment_data$year, 
+      #                   "<br>Catch: ", format(round(segment_data$catch/1000, 1), big.mark=","), 
+      #                   "K")
+    )
+  }
+  # Configure layout
+n_fishers_plot <- n_fishers_plot %>%
+  layout(yaxis = list(title = "Number of Fishers", zeroline = FALSE),  
+            xaxis = list(
+              title = "Year",
+              tickmode = "array",
+              tickvals = all_years,
+              ticktext = all_years,
+              tickangle = 0,
+              dtick = 1,
+              showline = FALSE
+            ), 
+            legend = list(x = 1, y = 1,
+            xanchor = "right",
+            yanchor = "top"))
+
+n_fishers_plot
+
+})
+
 run_sim <- eventReactive(sim_trigger(), {
   # Create empty results list to fill
   Results <- list() 
@@ -400,55 +476,77 @@ output$combined_plot <- renderPlotly({
 
   # Get all unique years from the data to use as breaks
   all_years <- sort(unique(plot_data$year))
+  segments <- unique(plot_data$type) 
 
-  plot_data$hover_text <- paste0("Year: ", plot_data$year, 
-                                  "<br>Catch: ", format(round(plot_data$catch/1000, 1), big.mark=","), 
-                                  "K")
-
-  tc.all$hover_text <- paste0("Year: ", tc.all$year, 
-                          "<br>Total catch: ", format(round(tc.all$catch/1000, 1), big.mark=","), 
-                          "K")
 
   # Create interactive plot with plot_ly
-  p <- plot_ly(plot_data, x = ~year, y = ~catch, color = ~type, 
-          type = "bar", 
-          colors = colors,
-          text = ~hover_text,
-          hoverinfo = "text") %>%     
-      add_trace(data = tc.all, x = ~year, y = ~catch, type = "scatter",
-          mode = "lines+markers",
-          line = list(color = "grey", width = 2),
-          marker = list(color = "grey", size = 3),
-          name = "Total catch used in the 2024 assessment", 
-          text = ~hover_text,
-          hoverinfo = "text"
-          ) %>%
-      layout(yaxis = list(title = "Catch (lbs)"), 
+  plot <- plot_data %>% 
+  group_by(year) %>% 
+  summarise(annual_total = sum(catch)) %>%
+  plot_ly() %>%
+  # Add the gray background bars for totals
+  add_trace(
+    type = "bar",
+    x = ~year,
+    y = ~annual_total,
+    name = "Total Catch",
+    marker = list(color = "#D3D3D3"),
+    width = 0.8,
+    hoverinfo = "text",
+    hovertext = ~paste0("Year: ", year, 
+                        "<br>Catch: ", format(round(annual_total/1000, 1), big.mark=","), 
+                        "K")) %>% 
+  add_trace(data = tc.all, x = ~year, y = ~catch, type = "scatter",
+            mode = "lines+markers", 
+            line = list(color = "grey", width = 2),
+            marker = list(color = "grey", size = 3),
+            name = "Total catch used in the 2024 assessment",
+            hoverinfo = "text",
+            hovertext = ~paste0("Year: ", year, 
+                        "<br>Catch: ", format(round(catch/1000, 1), big.mark=","), 
+                        "K")) 
+for (segment in segments) {
+
+  segment_data <- filter(plot_data, type == segment)
+
+  plot <- plot %>%
+    add_trace(
+      type = "bar",
+      x = segment_data$year,
+      y = segment_data$catch,
+      name = segment,
+      marker = list(color = colors[segment],
+                    line = list(color = "D3D3D3", width = 1)),
+      width = 0.2,
+      offset =  ifelse(segment == "Commercial - CML reported", -0.3, 
+                     ifelse(segment == "Non-commercial - BFVR approach", 0.1, -0.1)),
+      hoverinfo = "text",
+      hovertext = paste0("Year: ", segment_data$year, 
+                        "<br>Catch: ", format(round(segment_data$catch/1000, 1), big.mark=","), 
+                        "K")
+    )
+}
+
+  # Configure layout
+  plot <- plot %>%
+          layout(yaxis = list(title = "Catch (lbs)",
+            tickmode = "linear",
+            tick0 = 0,
+            dtick = 50000,
+            tickformat =  ",~s", zeroline = FALSE),  
             xaxis = list(
               title = "Year",
               tickmode = "array",
               tickvals = all_years,
               ticktext = all_years,
               tickangle = 0,
-              dtick = 1
+              dtick = 1,
+              showline = FALSE
             ), 
-            yaxis = list(
-            hoverformat = ".0f"
-            ),
             legend = list(x = 1, y = 1,
             xanchor = "right",
-            yanchor = "top"),
-            barmode = 'stack') %>%
-      # Add text annotations for the annual sum above each bar
-      add_annotations(
-        data = plot_data %>% group_by(year) %>% summarize(total = round(sum(catch), digits = -3)),
-        x = ~year,
-        y = ~total,
-        text = ~paste0(total/1000, "K"),
-        showarrow = FALSE,
-        yshift = 16,  # Adjust as needed to position text above bars
-        font = list(size = 14)
-      )
+            yanchor = "top"))
+  plot
 
 }) # end of Deep 7 catch plot
 
